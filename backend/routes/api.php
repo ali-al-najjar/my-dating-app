85f07b420a4df2aaf500a91f900842479510c741
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-18 11:00:04 +0200

    Dropped todo table

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,13 +12,6 @@ Route::controller(AuthController::class)->group(function () {
 
 });
 
-Route::controller(TodoController::class)->group(function () {
-    Route::get('todos', 'index');
-    Route::post('todo', 'store');
-    Route::get('todo/{id}', 'show');
-    Route::put('todo/{id}', 'update');
-    Route::delete('todo/{id}', 'destroy');
-}); 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
